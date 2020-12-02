63bdbf2ef372a6987c09aacf36285c4763b91823
Kevin Dixon <gaiwecoor@gmail.com>
2020-12-02 14:20:11 -0600

    Initial setup

---
+++ template.py
@@ -0,0 +1,28 @@
+#############
+##  SETUP  ##
+#############
+file = "input.txt"
+with open(file) as f:
+    data = f.read().splitlines()
+
+##############
+##  PART 1  ##
+##############
+def part1(data):
+    pass
+
+##############
+##  PART 2  ##
+##############
+def part2(data):
+    pass
+
+#################
+##  EXECUTION  ##
+#################
+
+print("\nPart 1:")
+print(part1(data))
+
+print("\nPart 2:")
+print(part2(data))
